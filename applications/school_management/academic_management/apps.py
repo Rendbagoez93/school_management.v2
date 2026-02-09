d9a83f289f23ee21526475a9509cb08c65e3f80f
kidmend58@gmail.com <kidmend58@gmail.com>
2026-02-09 17:55:25 +0700

    Apps Placeholder

---
+++ applications/school_management/academic_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademicManagementConfig(AppConfig):
+    name = 'applications.school_management.academic_management'
---
+++ applications/school_management/staff_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StaffManagementConfig(AppConfig):
+    name = 'applications.school_management.staff_management'
